e4fae0b61bfe791fc283fd20e2f84b273aa5ade0
Anindyadeep <Anindyadeep@users.noreply.github.com>
2023-02-07 16:01:13 +0000

    ✅ Ready to clone and code.

--- ateltaapi/app.py
+++ ateltaapi/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+ateltaapi API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="ateltaapi",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://ateltaapi.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "AteltaAI",
+        "url": "http://ateltaapi.com/contact/",
+        "email": "AteltaAI@ateltaapi.com",
     },
     license_info={
         "name": "The Unlicense",
--- ateltaapi/config.py
+++ ateltaapi/config.py
@@ -5,11 +5,11 @@ from dynaconf import Dynaconf
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 settings = Dynaconf(
-    envvar_prefix="project_name",
+    envvar_prefix="ateltaapi",
     preload=[os.path.join(HERE, "default.toml")],
     settings_files=["settings.toml", ".secrets.toml"],
     environments=["development", "production", "testing"],
-    env_switcher="project_name_env",
+    env_switcher="ateltaapi_env",
     load_dotenv=False,
 )
 
@@ -18,7 +18,7 @@ settings = Dynaconf(
 # How to use this application settings
 
 ```
-from project_name.config import settings
+from ateltaapi.config import settings
 ```
 
 ## Acessing variables
@@ -45,15 +45,15 @@ KEY=value
 
 ### As environment variables
 ```
-export PROJECT_NAME_KEY=value
-export PROJECT_NAME_KEY="@int 42"
-export PROJECT_NAME_KEY="@jinja {{ this.db.uri }}"
-export PROJECT_NAME_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
+export ateltaapi_KEY=value
+export ateltaapi_KEY="@int 42"
+export ateltaapi_KEY="@jinja {{ this.db.uri }}"
+export ateltaapi_DB__uri="@jinja {{ this.db.uri | replace('db', 'data') }}"
 ```
 
 ### Switching environments
 ```
-PROJECT_NAME_ENV=production project_name run
+ateltaapi_ENV=production ateltaapi run
 ```
 
 Read more on https://dynaconf.com
